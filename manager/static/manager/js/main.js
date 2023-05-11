254fa66cf005b80a72d9d00721337335c9fb642c
misha023 <m1kelyangio@gmail.com>
2023-05-11 23:47:41 +0300

    обновил_флексобксы_допилил_моб_меню_сделал_биржы_с_прокруткой'

--- manager/static/manager/js/main.js
+++ manager/static/manager/js/main.js
@@ -1,11 +1,11 @@
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
 
-
 const left_bar = document.getElementById("left-bar")
 const side_bar = document.getElementById("side-bar")
 const content_block = document.getElementById("content-block")
 const open_btn = document.getElementById("openSide_bar")
+const close_btn = document.getElementById("closeSide_bar")
 
 function openleft_bar() {
   left_bar.classList.add('active')
@@ -14,19 +14,21 @@ function closeleft_bar() {
   left_bar.classList.remove('active')
 }
 
-
+const label_toggle_btn = document.getElementById("form-body-label-btn");
+label_toggle_btn.addEventListener("click", () => label_toggle_btn.classList.toggle("active"));
 
 function openSide_bar() {
   side_bar.classList.remove('active')
   open_btn.classList.remove('active')
+  close_btn.classList.remove('active')
   content_block.classList.remove('active-content-block')
 }
 function closeSide_bar() {
   side_bar.classList.add('active')
   open_btn.classList.add('active')
+  close_btn.classList.add('active')
   content_block.classList.add('active-content-block')
 }
-
 
 
 

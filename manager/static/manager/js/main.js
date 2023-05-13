ef8a64ebaad2218cba4a0b0d949b04741dc28a94
misha023 <m1kelyangio@gmail.com>
2023-05-13 06:27:29 +0300

    добавил_профиль_в_хеадере__добавил_всплывающие_меню_поигрался_с_джаваскриптом

--- manager/static/manager/js/main.js
+++ manager/static/manager/js/main.js
@@ -1,18 +1,33 @@
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
 
+const addclient = document.getElementById("pop-add-client")
+
+const addbirzh = document.getElementById("pop-add-birzh")
+
 const left_bar = document.getElementById("left-bar")
 const side_bar = document.getElementById("side-bar")
 const content_block = document.getElementById("content-block")
 const open_btn = document.getElementById("openSide_bar")
 const close_btn = document.getElementById("closeSide_bar")
 
-function openleft_bar() {
-  left_bar.classList.add('active')
+function addclient_show() {
+  addbirzh.classList.remove('active')
+  addclient.classList.add('active')
 }
-function closeleft_bar() {
-  left_bar.classList.remove('active')
+function addclient_close() {
+  addclient.classList.remove('active')
 }
+
+function addbirzh_show() {
+  addbirzh.classList.add('active')
+  addclient.classList.remove('active')
+}
+function addbirzh_close() {
+  addbirzh.classList.remove('active')
+}
+
+
 
 const label_toggle_btn = document.getElementById("form-body-label-btn");
 label_toggle_btn.addEventListener("click", () => label_toggle_btn.classList.toggle("active"));
@@ -31,8 +46,8 @@ function closeSide_bar() {
 }
 
 
-
-
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }

f3d7d794dc3a2b1e9951f6db26bb937622a010a5
misha023 <m1kelyangio@gmail.com>
2023-05-17 23:03:30 +0300

    new

--- manager/static/manager/js/main.js
+++ manager/static/manager/js/main.js
@@ -1,6 +1,6 @@
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
-function myFunction() {
+function drop_show_header() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 

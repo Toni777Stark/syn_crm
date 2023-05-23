eda9109964f6f083543c115dc3b13755fbcd6054
misha023 <m1kelyangio@gmail.com>
2023-05-23 20:00:37 +0300

    update_manager-control

--- manager/static/manager-test/js/main.js
+++ manager/static/manager-test/js/main.js
@@ -1,10 +1,13 @@
 $(window).keyup(function(e){
-	var target = $('.checkbox-ios input:focus');
+	var target = $('.checkbox-label input:focus');
 	if (e.keyCode == 9 && $(target).length){
 		$(target).parent().addClass('focused');
 	}
 });
  
-$('.checkbox-ios input').focusout(function(){
+$('.checkbox-label input').focusout(function(){
 	$(this).parent().removeClass('focused');
 });
+
+
+

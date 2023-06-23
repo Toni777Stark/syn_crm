55607b0219f49fdd1c6fb046953ff56d2f83f7ec
misha023 <m1kelyangio@gmail.com>
2023-06-23 17:54:37 +0300

    new

--- manager/urls.py
+++ manager/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('info_order/', views.info_order, name="info_order"),
     path('save-data/', views.save_data, name="save_data"),
     path('refusals', views.refusals, name="refusals"),
-    path('refunds', views.refunds, name="refunds"),
+    path('refunds', views.index, name="refunds"),
 ]

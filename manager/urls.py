c9e9b3c1ca958dba34d031dd8f7fd62abbc92a63
misha023 <m1kelyangio@gmail.com>
2023-06-27 14:57:39 +0300

    messenger

--- manager/urls.py
+++ manager/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('info_order/', views.info_order, name="info_order"),
     path('save-data/', views.save_data, name="save_data"),
     path('refusals', views.refusals, name="refusals"),
-    path('refunds', views.index, name="refunds"),
+    path('refunds', views.refunds, name="refunds"),
+    path('clients', views.clients, name="clients"),
+    path('dashboard', views.dashboard, name="dashboard"),
 ]

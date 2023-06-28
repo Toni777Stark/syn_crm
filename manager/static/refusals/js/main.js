9e6f7d1e2b8fdd53e9ba2e762f42448f4a085f81
misha023 <m1kelyangio@gmail.com>
2023-06-28 15:12:20 +0300

    new

--- manager/static/refusals/js/main.js
+++ manager/static/refusals/js/main.js
@@ -1,3 +1,21 @@
+$(".zoom-level input").on('input', function () {
+	if ($(this).val().length > 2) {
+		$(this).val("100")
+	}
+});
+$(".zoom-level input").on('change', function () {
+	zoom($(this).val() + "%")
+	$(this).val("")
+});
+
+
+function zoom(input_value) {
+	$("html").css({
+		"zoom": input_value
+	})
+}
+
+
 $(window).keyup(function(e){
 	var target = $('.checkbox-label input:focus');
 	if (e.keyCode == 9 && $(target).length){
